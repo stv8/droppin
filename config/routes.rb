084520a42893b13c686abb90b2f157286dabba14
stv8 <stvillars8@gmail.com>
2014-07-29 17:20:30 -0500

    rolling back; name spacing tables is bad

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :spots
-      
+      resources :spots, except: [:new, :edit]
+      resources :users, except: [:new, :edit]
     end
   end
 
