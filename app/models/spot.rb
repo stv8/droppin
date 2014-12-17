1b2a51cb9ff549effa9340868dcaf6bb9091f6ab
stv8 <stvillars8@gmail.com>
2014-12-16 21:10:33 -0600

    some tweaks

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,7 +1,7 @@
 class Spot < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :photo, styles: { :medium => "200x200>", :thumb => "100x100>" }
+	has_attached_file :photo, styles: { :medium => '200x200>', :thumb => '100x100>' }
 	validates_attachment_content_type :photo, :content_type => /^image\/(png|gif|jpeg|jpg)/
 
 	def thumb_url
