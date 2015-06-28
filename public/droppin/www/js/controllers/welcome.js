31cf5a9272dcafd7421e25d996820f9225c354fe
Sean Villars <svillars@iparadigms.com>
2015-06-27 19:38:54 -0500

    refactor login screen

--- public/droppin/www/js/controllers/welcome.js
+++ public/droppin/www/js/controllers/welcome.js
@@ -1,31 +1,43 @@
-app.controller('WelcomeCtrl', function($scope, $state, Helpers, Session, CurrentUser) {
-
+app.controller('WelcomeCtrl', function($scope, $state, Helpers, Session, Registration, CurrentUser, $ionicPopup) {
     Helpers.redirect_if_authenticated();
 
-    $scope.signin = { email: "", password: ""};
+    // if login is true, show login form
+    // if login is false, show register form
+    $scope.login = true;
+
+    $scope.toggleLogin = function() {
+        $scope.login = !$scope.login;
+    };
+
+    $scope.credentials = { email: "", password: ""};
 
     $scope.returnToWelcome = function() {
         $state.go('welcome');
     };
 
-    $scope.signIn = function() {
+    $scope.submit = function() {
+        console.log($scope.login);
         Helpers.show_loading();
-
-        Session.save(
-            { session: $scope.signin },
-            function(response) {
-                Helpers.hide_loading();
-                var user = response.user;
-                CurrentUser.store(user.authentication_token, user.email);
-                $state.go('tab.spots');
-            },
-            function(response) {
-                Helpers.ajax_error_handling(response);
-            }
-        );
+        if($scope.login) {
+            Session.save({ session: $scope.credentials }, success, error);
+        } else {
+            Registration.save($scope.credentials, success, error);
+        }
     };
 
-    $scope.go_to_sign_up = function() {
-        $state.go('sign_up');
-    };
+    function success(response) {
+        Helpers.hide_loading();
+        var user = response.user;
+        CurrentUser.store(user.authentication_token, user.email);
+        $state.go('tab.spots');
+    }
+
+    function error(response) {
+        Helpers.hide_loading();
+        // Helpers.ajax_error_handling(response);
+        var alertPopup = $ionicPopup.alert({
+            title: 'Login failed!',
+            template: 'Please check your credentials!'
+        });
+    }
 });
