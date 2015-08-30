0fa15f1fe07c1dd3878e8d42aa556378da97b638
Sean Villars <stvillars8@gmail.com>
2015-08-30 16:14:19 -0500

    Add grid view for spots

--- public/droppin/www/js/controllers/spots.js
+++ public/droppin/www/js/controllers/spots.js
@@ -2,4 +2,10 @@ app.controller('SpotsCtrl', function($scope, Spot) {
 
     $scope.spots = Spot.query();
 
+    $scope.gridView = false;
+
+    $scope.toggleGrid = function() {
+        $scope.gridView = !$scope.gridView;
+    };
+
 });
